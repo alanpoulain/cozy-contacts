f9b77c1b2f7193befa9e6341cbab87febce316fe
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-09-30 16:31:39 +0200

    feat: Add contact list filter by groups

--- src/components/ContentResult.jsx
+++ src/components/ContentResult.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PropTypes } from 'prop-types'
+import filter from 'lodash/filter'
+import get from 'lodash/get'
 
-import { isQueryLoading } from 'cozy-client'
+import { isQueryLoading, useQuery, hasQueryBeenLoaded } from 'cozy-client'
 import { Content } from 'cozy-ui/transpiled/react/Layout'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 
 import Header from './Header'
 import Toolbar from './Toolbar'
@@ -10,19 +13,46 @@ import ContactsList from './ContactsList/ContactsList.jsx'
 import SpinnerContact from './Common/Spinner'
 import { reworkContacts } from '../helpers/contacts'
 
+import ContactGroups from './ContactCard/ContactGroups'
+import { queryAllGroups } from '../helpers/queries'
+
 export const ContentResult = ({
   hasServiceBeenLaunched,
   contactsWithIndexesResult,
   contactsWithNoIndexesResult
 }) => {
+  const { t } = useI18n()
+  const allContactsFilter = {
+    name: t('filter.all-contacts'),
+    withNoAction: true,
+    isGroup: false
+  }
+  const [selectedGroup, setSelectedGroup] = useState(allContactsFilter)
+
+  const resultAllGroups = useQuery(
+    queryAllGroups.definition,
+    queryAllGroups.options
+  )
+
   const dataHaveBeenLoaded =
     !isQueryLoading(contactsWithIndexesResult) &&
     !isQueryLoading(contactsWithNoIndexesResult) &&
     !contactsWithIndexesResult.hasMore &&
-    !contactsWithNoIndexesResult.hasMore
+    !contactsWithNoIndexesResult.hasMore &&
+    (!isQueryLoading(resultAllGroups) || hasQueryBeenLoaded(resultAllGroups))
 
   if (!dataHaveBeenLoaded)
     return <SpinnerContact size="xxlarge" loadingType="fetching_contacts" />
+
+  const selecteGroup = selectedGroup => {
+    setSelectedGroup(selectedGroup)
+  }
+
+  const resetFilter = group => {
+    if (get(group, '_id') === get(selectedGroup, '_id')) {
+      setSelectedGroup(allContactsFilter)
+    }
+  }
 
   const contacts = reworkContacts(
     hasServiceBeenLaunched,
@@ -30,11 +60,32 @@ export const ContentResult = ({
     contactsWithNoIndexesResult.data
   )
 
+  let filteredContacts = contacts
+
+  if (selectedGroup._id) {
+    const filters = ['groups', { data: [selectedGroup] }]
+    filteredContacts = filter(contacts, filters)
+  }
+
   return (
     <>
-      {contacts.length >= 1 && <Header right={<Toolbar />} />}
+      {contacts.length >= 1 && (
+        <Header
+          left={
+            <ContactGroups
+              allGroups={resultAllGroups.data}
+              value={selectedGroup}
+              onChange={selecteGroup}
+              noOptionsMessage={() => t('filter.none')}
+              preliminaryOptions={[allContactsFilter]}
+              onGroupDeletion={resetFilter}
+            />
+          }
+          right={<Toolbar />}
+        />
+      )}
       <Content>
-        <ContactsList contacts={contacts} />
+        <ContactsList contacts={filteredContacts} />
       </Content>
     </>
   )
