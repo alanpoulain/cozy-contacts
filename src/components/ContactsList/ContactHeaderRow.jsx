e42432736c3944e83d67622edf65127bc4dc81a2
Alan Poulain <contact@alanpoulain.eu>
2020-10-09 12:59:25 +0200

    feat: display the highlighted contact in header row and scroll into it when the first letter is entered

--- src/components/ContactsList/ContactHeaderRow.jsx
+++ src/components/ContactsList/ContactHeaderRow.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 
-const ContactHeaderRow = props => <div className="divider">{props.header}</div>
+import HighlightedContactContext from '../Contexts/HighlightedContact'
+
+const ContactHeaderRow = ({ header }) => {
+  const { highlightedContact } = useContext(HighlightedContactContext)
+  const dividerRef = useRef(null)
+
+  useEffect(() => {
+    if (
+      dividerRef &&
+      highlightedContact.length === 1 &&
+      highlightedContact.slice(0, 1) === header
+    ) {
+      // go as high as posible to make sure the fixed divider doesn't hide the first contact row
+      dividerRef.current.scrollIntoView(false)
+      dividerRef.current.scrollIntoView()
+    }
+  }, [highlightedContact, header])
+
+  return (
+    <div className="divider" ref={dividerRef}>
+      {highlightedContact.length > 0 &&
+      highlightedContact.slice(0, 1) === header ? (
+        <b>{highlightedContact}</b>
+      ) : (
+        header
+      )}
+    </div>
+  )
+}
 
 ContactHeaderRow.propTypes = {
   header: PropTypes.string.isRequired
